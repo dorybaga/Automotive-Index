3584f7847fa8201bd4c8b8ad54121930207eaed8
Dory Baga <dorybaga@Dorys-MacBook-Pro.local>
2017-07-22 16:54:50 -1000

    select make_code, model_code, mode_title, year where car_models is LAM

--- indexing.sql
+++ indexing.sql
@@ -7,24 +7,36 @@ CREATE DATABASE indexed_cars WITH OWNER indexed_cars_user;
 \c indexed_cars
 
 \i scripts/car_models.sql
-\i scripts/car_model_data.sql
-\i scripts/car_model_data.sql
-\i scripts/car_model_data.sql
-\i scripts/car_model_data.sql
-\i scripts/car_model_data.sql
-\i scripts/car_model_data.sql
-\i scripts/car_model_data.sql
-\i scripts/car_model_data.sql
-\i scripts/car_model_data.sql
-\i scripts/car_model_data.sql
 
+\i scripts/car_model_data.sql
+\i scripts/car_model_data.sql
+\i scripts/car_model_data.sql
+\i scripts/car_model_data.sql
+\i scripts/car_model_data.sql
+\i scripts/car_model_data.sql
+\i scripts/car_model_data.sql
+\i scripts/car_model_data.sql
+\i scripts/car_model_data.sql
+\i scripts/car_model_data.sql
 
 \timing
 
-SELECT DISTINCT make_title FROM car_models where make_code = 'LAM';
+SELECT DISTINCT make_title FROM car_models
+WHERE make_code = 'LAM';
 -- make_title: lamborghini
--- time: 34.411 ms
+-- time: 25.426 ms
+
+SELECT DISTINCT model_title FROM car_models
+WHERE make_code = 'NISSAN' AND model_code = 'GT-R';
+-- time: 21.771 ms
+
+SELECT make_code, model_code, model_title, year FROM car_models
+WHERE make_code = 'LAM';
+-- time 28.476 ms
 
 
 
+
+
+\timing
 \c dorybaga
